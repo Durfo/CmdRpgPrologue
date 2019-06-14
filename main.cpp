5c5b2ae87609850c58c28657d9c19dbf3920eded
Wilfredo Santa Maria <Simple@Davids-Air-2.fios-router.home>
2019-06-13 22:04:00 -0700

    second commit

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+
+#include "Player.h"
+
+
+using namespace std;
+
+string questions(string question, string answerA
+              string answerB, string answerC,
+              string answerD, string replyA,
+              string replyB, string replyC,
+              string replyD)
+{
+
+  bool loop = true;
+  string invalid = "Please enter a valid answer";
+  string input;
+
+  cout << question << endl;
+
+  while(loop)
+  {
+    cin >> input;
+    if(input == answerA)
+    {
+      cout << replyA << endl;
+      return answerA;
+
+    }
+    else if(input == answerB)
+    {
+      cout << replyB << end;
+      return answerB;
+    }
+    else if (input == answerC)
+    {
+      cout << replyC << endl;
+      return answerC;
+    }
+    else if(input == answerD)
+    {
+      cout << replyD << endl;
+      return answerD;
+    }
+    else
+    {
+      cout << invalid << endl;
+
+    }
+  }
+}
+int main()
+{
+
+  cout << p1.returnhp() <<end1;
+}
+
+
+
+
+
+}
